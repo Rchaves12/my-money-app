f28aa304674026b9b086f336ea4f14d400b68473
chaves <chaves@tce.al.gov.br>
2019-01-03 11:34:20 -0300

    criação do menu lateral do sistema

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,45 +3,45 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
+    output: {
         path: __dirname + '/public',
-        filename:'./app.js'
+        filename: './app.js'
     },
-    devServer:{
-        port:8080,
+    devServer: {
+        port: 8080,
         contentBase: './public',
     },
-    resolve:{
-        extensions: ['','.js', '.jsx'],
-        alias:{
-            modules: __dirname + '/node_modeules',
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-            bootstrap: 'modeules/admin-lte/bootstrap/js/bootstrap.js'
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins:[
+    plugins: [ 
         new webpack.ProvidePlugin({
-            $:'jquery',
-            jQuery:'jquery',
+            $: 'jquery',
+            jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
     ],
-    module:{
-        loaders:[{
-            test:/.js[x]?$/,
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
             loader: 'babel-loader',
-            exclude:/node_modeules/,
+            exclude: /node_modules/,
             query: {
-                presets:['es2015','react'],
-                plugins:['transform-object-rest-spread']
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        },{
-            teste:/\.css$/,
-            loader:ExtractTextPlugin.extract('style-loader','css-loader')
-        },{
-            test:/\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-            loader:'file'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
         }]
     }
 }
