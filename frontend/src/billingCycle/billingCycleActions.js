c7e924d05c01709fbd031ec6d536f719a529fa93
chaves <chaves@tce.al.gov.br>
2019-01-09 08:42:33 -0300

    alterar e excluir ciclos de pagamento

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm, initialize} from 'redux-form'
+import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -14,23 +15,30 @@ export function getList() {
 }
 
 export function create(values){
+    return submit(values, 'post')
+    
+   
+}
+
+export function update(values){
+    return submit(values, 'put')
+}
+export function remove(values){
+    return submit(values, 'delete')
+}
+
+function submit(values, method){
     return dispacth => {
-        axios.post(`${BASE_URL}/billingCycles`, values)
+        const id = values._id ? values._id : ''
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
             .then(resp =>{
                 toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-                dispacth([
-                    resetForm('billingCycleForm'),
-                    getList(),
-                    selectTab('tabList'),
-                    showTabs('tabList','tabCreate')
-                ])
+                dispatch(init())
             })
             .catch(e =>{
                 e.response.data.errors.forEach(error => toastr.error('Erro', error))
             })
     }
-    
-   
 }
 
 export function showUpdate(billingCycle){
@@ -38,5 +46,22 @@ export function showUpdate(billingCycle){
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingCycle)
+    ]
+}
+//#TODO Criar metodo generico 
+export function showDelete(billingCycle){
+    return [
+        showTabs('tabDelete'),
+        selectTab('tabDelete'),
+        initialize('billingCycleForm', billingCycle)
+    ]
+}
+
+export function init() {
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
     ]
 }
--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
+
+import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
     render(){
-        const { handleSubmit } = this.props
+        const { handleSubmit, readOnly } = this.props
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} 
+                    <Field name='name' component={labelAndInput} readOnly={readOnly}
                     label='Nome' cols='12 4' placeholder='Informe o Nome'/>
-                    <Field name='month' cols='12 4' component= {labelAndInput} 
+                    <Field name='month' cols='12 4' component= {labelAndInput} readOnly={readOnly}
                     label='Mês' type='number' placeholder='Informe o Mês'/>
-                    <Field name='year' component={labelAndInput} 
+                    <Field name='year' component={labelAndInput} readOnly={readOnly}
                     label='Ano' type='number' cols='12 4' placeholder='Informe o Ano' />
                 </div>
                 <div className='box-footer'>
                     <button type="submit" className='btn btn-primary'>Submit</button>
+                    <button type='button' className='btn btn-default'
+                        onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
     }
 }
 
-export default reduxForm({form: 'billingCycleForm', destroyOnUnmount:false })(BillingCycleForm)
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
