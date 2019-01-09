9fdc390ee561ec4eff5ea6456dfe8e2454a804f8
chaves <chaves@tce.al.gov.br>
2019-01-09 16:19:55 -0300

    criação do novos campos do formulario

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,8 @@ import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
-import labelAndInput from '../common/form/labelAndInput'
+import LabelAndInput from '../common/form/labelAndInput'
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
     render(){
@@ -12,12 +13,13 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} readOnly={readOnly}
-                    label='Nome' cols='12 4' placeholder='Informe o Nome'/>
-                    <Field name='month' cols='12 4' component= {labelAndInput} readOnly={readOnly}
-                    label='Mês' type='number' placeholder='Informe o Mês'/>
-                    <Field name='year' component={labelAndInput} readOnly={readOnly}
-                    label='Ano' type='number' cols='12 4' placeholder='Informe o Ano' />
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
+                        label='Nome' cols='12 4' placeholder='Informe o Nome'/>
+                    <Field name='month' cols='12 4' component= {LabelAndInput} readOnly={readOnly}
+                        label='Mês' type='number' placeholder='Informe o Mês'/>
+                    <Field name='year' component={LabelAndInput} readOnly={readOnly}
+                        label='Ano' type='number' cols='12 4' placeholder='Informe o Ano' />
+                    <CreditList cols='12 6' />
                 </div>
                 <div className='box-footer'>
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>
---
+++ frontend/src/billingCycle/creditList.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Grid from '../common/layout/grid'
+import { Field } from "redux-form";
+
+import Input from '../common/form/input'
+
+class CreditList extends Component{
+    renderRows(){
+        return(
+            <tr>
+                <td><Field component={Input} name='credits[0].name' placeholder='Informe o Nome' /></td>
+                <td><Field component={Input} name='credits[0].value' type='number' placeholder='Informe o Valor' /></td>
+                <td>
+                    <button type='button' className='btn btn-sucess'><i className="fa fa-plus"></i></button>
+                    <button type='button' className='btn btn-warning'><i className="fa fa-clone"></i></button>
+                    <button type='button' className='btn btn-danger'><i className="fa fa-trash-o"></i></button>
+                </td>
+            </tr>
+        )
+    }
+    render() {
+        return (
+            <Grid cols={this.props.cols}>
+                <fieldset>
+                    <legend>Créditos</legend>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
+                    </table>
+                </fieldset>
+            </Grid>
+        )
+    }
+}
+
+export default CreditList
